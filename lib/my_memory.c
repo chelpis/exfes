d2696fdf0d7a4e8998963c65320cf7fde9efbdd6
Jong-Shian Wu <js@jong.sh>
2018-05-18 08:31:03 +0800

    update exfes_calloc() from recursion to iteration
    not all implementation support tail call optimization

--- lib/my_memory.c
+++ lib/my_memory.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 void *exfes_calloc(size_t num, size_t size, size_t num_max_retries) {
-  void *p = calloc(num, size);
-  if (p)
-    return p;
-  if (num_max_retries == 0)
-    return 0;
-  return exfes_calloc(num, size, num_max_retries - 1);
+  while (1) {
+    void *p = calloc(num, size);
+    if (p)
+      return p;
+    if (num_max_retries == 0)
+      return 0;
+    num_max_retries -= 1;
+  }
 }
