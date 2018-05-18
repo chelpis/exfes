ec35ae2d08562b367b8585731628e0f7c31cc23b
Jong-Shian Wu <js@jong.sh>
2018-05-19 01:30:51 +0800

    update reformat script

--- reformat-the-source-files-in-lib.sh
+++ reformat-the-source-files-in-lib.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-find lib -type f -name '*.[ch]' ! -name binomials.c -exec echo clang-format -style='{BasedOnStyle: WebKit, IndentWidth: 4, ContinuationIndentWidth: 8, SpacesBeforeTrailingComments: 2, PointerAlignment: Right}' -i {} ';'
+clang-format -style='{BasedOnStyle: WebKit, IndentWidth: 4, ContinuationIndentWidth: 8, SpacesBeforeTrailingComments: 2, PointerAlignment: Right}' -i lib/*.[ch]
