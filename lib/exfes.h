5c01a09935edfa2b421b251aa26e12e7615e5ec7
Jong-Shian Wu <js@jong.sh>
2018-05-21 08:02:38 +0800

    add extern "C" to exfes(...) function declaration to make it easier for C++ users

--- lib/exfes.h
+++ lib/exfes.h
@@ -9,6 +9,14 @@
 #include <stdbool.h>  // bool
 #include <stdint.h>  // uint8_t, uint32_t, uint64_t
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int exfes(uint32_t numFixedVariables, uint32_t numVariables, uint32_t numEquations, uint64_t startPointHigh, uint64_t startPointLow, const uint8_t *coefficientsMatrix, bool (*shouldAbortNow)(void), uint64_t *solutionHigh, uint64_t *solutionLow);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // EXFES_H_INCLUDED
